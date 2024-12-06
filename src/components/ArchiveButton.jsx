0ded6dcd7906f0c7d755ac459ce3146f03a2e944
alfimonth <alvy1610@gmail.com>
2024-12-06 14:47:26 +0700

    feat: react icons

--- src/components/ArchiveButton.jsx
+++ src/components/ArchiveButton.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import { MdUnarchive } from "react-icons/md";
+import { MdArchive } from "react-icons/md";
+import React from "react";
 
 function ArchiveButton({ id, archived, onArchive }) {
-  return <button className='note-item__archive' onClick={() => onArchive(id)}>
-    {archived ? 'Pindahkan' : 'Arsipkan'}
-  </button>
+  return (
+    <button
+      className="note-item__archive"
+      onClick={() => onArchive(id)}
+      title={archived ? "Keluarkan dari Arsip" : "Arsipkan"}
+    >
+      {archived ? <MdUnarchive /> : <MdArchive />}
+    </button>
+  );
 }
 
 export default ArchiveButton;
--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import { MdDelete } from "react-icons/md";
+import React from "react";
 
 function DeleteButton({ id, onDelete }) {
-  return <button className='note-item__delete' onClick={() => onDelete(id)}>Hapus</button>
+  return (
+    <button className="note-item__delete" onClick={() => onDelete(id)} title="Hapus Catatan">
+      <MdDelete />
+    </button>
+  );
 }
 
 export default DeleteButton;
